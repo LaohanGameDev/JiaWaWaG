1859f26f7b06094900ff82c0a8b650b465343cdc
jchans <jiangchenghans@gmail.com>
2021-01-10 22:57:57 +0800

    first commit to archive this project

---
+++ Assets/Scripts/ClawFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
